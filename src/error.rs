3c7d3c2fe2692d511d56e89859d704f52f476896
terryllowery <tlowe33@my.wgu.edu>
2025-01-23 12:03:50 -0600

    Setup error reporting

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,8 @@
 //! Top level error types
+
+#[derive(Debug, thiserror::Error)]
+#[error("an application error occurred")]
+pub struct AppError;
+
+/// A suggestion displayed to the user
+pub struct Suggestion(pub &'static str);
--- src/init.rs
+++ src/init.rs
@@ -1,1 +1,24 @@
 //! Application Setup
+
+use crate::error::Suggestion;
+use clap_verbosity_flag::Verbosity;
+use error_stack::{fmt::ColorMode, Report};
+use owo_colors::OwoColorize;
+use tracing_log::AsTrace;
+use tracing_subscriber::EnvFilter;
+
+pub fn error_reporting() {
+    Report::set_color_mode(ColorMode::Color);
+    Report::install_debug_hook::<Suggestion>(|value, context| {
+        let msg = value.0;
+        let body = format!("suggestion: {}", msg);
+
+        match context.color_mode() {
+            ColorMode::Color => context.push_body(body.cyan().to_string()),
+            ColorMode::Emphasis => context.push_body(body.italic().to_string()),
+            ColorMode::None => context.push_body(body),
+        }
+    });
+
+    
+}
