25ca2d7770225ba3400981492a6dacf88c0a30c5
terryllowery <tlowe33@my.wgu.edu>
2025-01-23 15:36:50 -0600

    setup tracing

--- src/bin/track.rs
+++ src/bin/track.rs
@@ -1,9 +1,11 @@
 use error_stack::{Report, Result, ResultExt};
+use tracing::{info, instrument, warn};
 use track::{error::{AppError, Suggestion}, init};
+
 
 fn main() -> Result<(), AppError> {
     init::error_reporting();
-
+    init::tracing();
 
     Ok(())
     
--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,7 @@
 //! Application Setup
 
 use crate::error::Suggestion;
+use clap::builder;
 use clap_verbosity_flag::Verbosity;
 use error_stack::{fmt::ColorMode, Report};
 use owo_colors::OwoColorize;
@@ -18,7 +19,17 @@ pub fn error_reporting() {
             ColorMode::Emphasis => context.push_body(body.italic().to_string()),
             ColorMode::None => context.push_body(body),
         }
-    });
+    }); 
+}
 
-    
+pub fn tracing() {
+    use tracing_error::ErrorLayer;
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
+
+    tracing_subscriber::registry()
+    .with(tracing_subscriber::fmt::layer().pretty())
+    .with(EnvFilter::builder().from_env_lossy())
+    .with(ErrorLayer::default())
+    .init();
 }
